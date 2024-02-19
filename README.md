568d472d8c8204c4299a508393a10f7ebbcae264
Alejandro Ruiz <alejandroruizprieto@alumnos.tuniversformacion.es>
2024-02-19 10:13:14 +0100

    Comandos arreglados

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Primer proyecto de react
 **Autor** Alejandro Ruiz Prieto
+
+## A la hora de instalar realizar el comandod npm install 
+## npm start para levantar la web local
+## npm install -g eslint
+## eslint --init
--- index.jsx
+++ index.jsx
@@ -1,2 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <div>My WishList from REACT</div>,
+    document.getElementById('root'),
+);
